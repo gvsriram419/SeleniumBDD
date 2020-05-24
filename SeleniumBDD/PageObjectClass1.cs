4868b35b1f4056b96432598c9f87c6c936a0b46f
sreeram murthy <rammrth86@gmail.com>
2020-05-24 13:50:10 +0530

    Selenium Launch code

---
+++ SeleniumBDD/PageObjectClass1.cs
@@ -0,0 +1,45 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Support.PageObjects;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SeleniumBDD
+{
+    public  class PageObjectClass1
+    {
+ 
+        IWebDriver driver;
+        By Titlechecker = By.XPath("(//a[@title='Sign in to Guru'])[1]");
+        By Login = By.XPath("//*[@id='app']/div/div/div/div[1]/div/form/input[1]");
+        By Password = By.Name("password");
+        By Submit = By.XPath("//button[contains(text(),'Log in')]");
+
+        //Contructor
+        public PageObjectClass1(IWebDriver driver)
+        {
+            this.driver = driver;
+        }
+        public void TitleCheck()
+        {
+            driver.FindElement(Titlechecker).Click();
+        }
+
+        public void LoginCheck()
+        {
+            driver.FindElement(Login).SendKeys("Hello");
+        }
+
+        public void PasswordCheck()
+        {
+            driver.FindElement(Password).SendKeys("HelloSaiRam");
+        }
+
+        public void SubmitCheck()
+        {
+            driver.FindElement(Submit).Click();
+        }
+
+
+    }
+}
---
+++ SeleniumBDD/Program.cs
@@ -0,0 +1,89 @@
+﻿using Google/**/.Protobuf.WellKnownTypes;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+
+namespace SeleniumBDD
+{
+   
+    [TestClass]
+    public class Program
+    {
+       // IWebDriver driver;
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("I am Here");
+            IWebDriver driver = new ChromeDriver();
+            PageObjectClass1 page = new PageObjectClass1(driver);
+            driver.Navigate().GoToUrl("https://help.getguru.com/articles/cMEdM8Mi/Setting-up-Board-Permissions");
+            driver.Manage().Window.Maximize();
+            driver.Manage().Timeouts().ImplicitWait.TotalSeconds.Equals(50);
+            page.TitleCheck();
+            page.LoginCheck();
+            page.PasswordCheck();
+            page.SubmitCheck();
+            driver.Quit()
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+
+}
